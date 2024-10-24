a501baa158b0b1307dfdd0261edd4d40e0a77d39
bobbest <banditben.1994@gmail.com>
2024-10-24 23:12:28 +0800

    cloned master branch

---
+++ src/app/page.js
@@ -0,0 +1,54 @@
+'use client';
+import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <HomeMenu />
+      <section id="about" className="text-center my-16">
+        <SectionHeaders
+          subHeader={'Our story'}
+          mainHeader={'About us'}
+        />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex
+        flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit.
+            Magni minima odit recusandae.
+            Illum ipsa non repudiandae?
+            Eum ipsam iste quos suscipit tempora?
+            Aperiam esse fugiat inventore laboriosam
+            officiis quam rem!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit.
+            Magni minima odit recusandae.
+            Illum ipsa non repudiandae?
+            Eum ipsam iste quos suscipit tempora?
+            Aperiam esse fugiat inventore laboriosam
+            officiis quam rem!
+          </p>
+        </div>
+      </section>
+      <section id="contact" className="text-center my-8">
+        <SectionHeaders
+          subHeader={'Don\'t hesitate'}
+          mainHeader={'Contact us'}
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500"
+            href="tel:+46738123123">
+            +46 738 123 123
+          </a>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/components/layout/EditableImage.js
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import toast from "react-hot-toast";
+
+export default function EditableImage({ link, setLink }) {
+
+  async function handleFileChange(ev) {
+    const files = ev.target.files;
+
+    const uploadPromise = new Promise(async (resolve, reject) => {
+      if (files?.length === 1) {
+        const data = new FormData;
+
+        // validate file type
+        const file = files[0];
+        console.log(file);
+        if (file.type != "image/png" && file.type != "image/jpeg") {
+          reject('Invalid file type. Image must be in PNG or JPEG format only.');
+          return;
+        }
+
+        data.set('file', files[0]);
+
+        const response = await fetch('/api/upload', {
+          method: 'POST',
+          body: data,
+        });
+        if (response.ok) {
+          resolve();
+          return response.json().then(link => setLink(link));
+        } else {
+          reject("Error uploading image");
+        }
+      } else {
+        return;
+      }
+    });
+
+    await toast.promise(uploadPromise, {
+      loading: 'Uploading...',
+      success: 'Upload complete',
+      error: uploadPromise.catch((error) => error),
+    });
+  }
+
+  return (
+    <>
+      {link && (
+        <Image className="rounded-lg w-full h-full mb-1" src={link} width={250} height={250} alt={'avatar'} />
+      )}
+      {!link && (
+        <div className="text-center bg-gray-200 p-4 text-gray-500 rounded-lg mb-1">
+          No image
+        </div>
+      )}
+      <label>
+        <input type="file" className="hidden" onChange={handleFileChange} />
+        <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">Change image</span>
+      </label>
+    </>
+  );
+}
