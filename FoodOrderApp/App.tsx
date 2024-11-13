0ed38a85b9b0dab77df9a6a35b308bfc87f9707f
kaiweitan <kaiwei.tan@ncs.com.sg>
2024-11-13 21:37:38 +0800

    final commit for mobile app

--- FoodOrderApp/App.tsx
+++ FoodOrderApp/App.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? '#000000' : '#ffffff', // Adjust based on your color scheme
+    backgroundColor: isDarkMode ? '#000000' : '#ffffff',
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: backgroundStyle.backgroundColor }}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      {/* Render the Home component */}
-      {/* <Home /> */}
       <WebView 
-        source={{ uri: '192.168.50.18:3000' }} // change ipaddress accordingly
+        source={{ uri: 'https://192.168.50.18:3000' }} // Use your correct IP address
         style={{ flex: 1 }}
+        javaScriptEnabled={false} // protect against xss attacks
+        domStorageEnabled={true}
+        thirdPartyCookiesEnabled={false} // do not third-party cookies for Android
+        mixedContentMode="never" // only allow https
+        onError={(syntheticEvent) => {
+         const { nativeEvent } = syntheticEvent;
+         console.warn('WebView error: ', nativeEvent);
+        }}
       />
     </SafeAreaView>
   );
